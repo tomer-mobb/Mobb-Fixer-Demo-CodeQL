41f09eb35e19b22b1ce37fb401b88bbcb5e518bb
tomer-mobb <132216976+tomer-mobb@users.noreply.github.com>
2023-12-21 19:54:22 +0700

    Update xss-example.js

--- xss-example.js
+++ xss-example.js
@@ -3,6 +3,6 @@
 var urlParams = new URLSearchParams(window.location.search);
 var username = urlParams.get('username');
 
-document.write("Hello to you, " + username + "!!!");
+document.write("Hello to you, " + username + "!!!!");
 
 
