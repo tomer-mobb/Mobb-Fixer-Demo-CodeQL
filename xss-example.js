77133551d8ed865d4ac89aeeb669b675a38f8f4f
Haggai Yaniv <yhaggai@gmail.com>
2023-12-27 14:41:10 +0200

    test

--- xss-example.js
+++ xss-example.js
@@ -1,6 +1,6 @@
 // This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
 
 var urlParams = new URLSearchParams(window.location.search);
-var username = urlParams.get('username');
+var username = urlParams.get("username");
 
-
+document.write("Hello to you!!!!, " + username + "!!!");
